fac58091512ae8b055eb455bb169dfc2cb6e5a25
jrwyman <jacobwyman1995@gmail.com>
2021-01-09 18:13:20 -0700

    display current price inside app

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import CurrentPrice from './current-price/current-price';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      React is Working!
+      <CurrentPrice />
+      <div className="api-credit-message">
+        Powered by
+        <a href="https://www.coindesk.com/price/bitcoin"> CoinDesk</a>
+      </div>
     </div>
   );
 }
