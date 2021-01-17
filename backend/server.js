5af4de195b2605bbdb30586cb7b7beba2557b7ed
jrwyman <jacobwyman1995@gmail.com>
2021-01-17 01:47:59 -0700

    deploy-test-4

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,12 @@ const api = require('./routes/api');
 
 const PORT = (process.env.PORT || 5000);
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
 app.use('/api', api);
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Vary', 'Origin');
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
